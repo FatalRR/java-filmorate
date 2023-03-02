9c9862372c827f3e5f2b336b5d403dbfa7b24a8e
Sergey Malyunov <FatalRR@yandex.ru>
2023-03-02 20:02:39 +0700

    refactor: change for new abstract class

--- src/main/java/ru/yandex/practicum/filmorate/service/film/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/film/FilmService.java
@@ -1,13 +1,15 @@
 package ru.yandex.practicum.filmorate.service.film;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.excepions.NotFoundException;
 import ru.yandex.practicum.filmorate.excepions.ValidationException;
 import ru.yandex.practicum.filmorate.messages.LogMessages;
 import ru.yandex.practicum.filmorate.messages.ValidationExceptionMessages;
 import ru.yandex.practicum.filmorate.model.Film;
-import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
+import ru.yandex.practicum.filmorate.service.AbstractService;
+import ru.yandex.practicum.filmorate.storage.Storage;
 
 import java.time.LocalDate;
 import java.util.Comparator;
@@ -16,62 +18,41 @@ import java.util.stream.Collectors;
 
 @Service
 @Slf4j
-public class FilmService {
-    private final FilmStorage filmStorage;
+public class FilmService extends AbstractService<Film> {
+    @Autowired
+    public FilmService(Storage<Film> storage) {
+        super.storage = storage;
+    }
+
     private static final LocalDate BOUNDARY_DATE = LocalDate.of(1895, 12, 28);
     private static final Comparator<Film> LIKES_COMPARATOR = (o1, o2) -> Integer.compare(o2.getLikes().size(), o1.getLikes().size());
 
-    public FilmService(FilmStorage filmStorage) {
-        this.filmStorage = filmStorage;
-    }
-
-    private Film validate(Film film) {
+    @Override
+    public Film validate(Film film) {
         if (film.getReleaseDate().isBefore(BOUNDARY_DATE)) {
             throw new ValidationException(ValidationExceptionMessages.RELEASE_DATE.toString());
         }
         return film;
     }
 
-    public List<Film> getAll() {
-        return filmStorage.getAll();
-    }
-
-    public Film save(Film film) {
-        validate(film);
-        return filmStorage.save(film);
-    }
-
-    public Film update(Film film) {
-        validate(film);
-        return filmStorage.update(film);
-    }
-
-    public Film getById(int id) {
-        return filmStorage.getById(id);
-    }
-
-    public Film remove(Film film) {
-        return filmStorage.remove(film);
-    }
-
     public Film addLike(int filmId, int userId) {
         getById(filmId);
-        filmStorage.getById(filmId).getLikes().add(userId);
+        storage.getById(filmId).getLikes().add(userId);
         log.info(String.valueOf(LogMessages.LIKE_DONE), userId, filmId);
-        return filmStorage.getById(filmId);
+        return storage.getById(filmId);
     }
 
     public Film removeLike(int filmId, int userId) {
         getById(filmId);
-        if (!filmStorage.getById(filmId).getLikes().contains(userId))
+        if (!storage.getById(filmId).getLikes().contains(userId))
             throw new NotFoundException(String.valueOf(ValidationExceptionMessages.WITHOUT_LIKE));
-        filmStorage.getById(filmId).getLikes().remove(userId);
+        storage.getById(filmId).getLikes().remove(userId);
         log.info(String.valueOf(LogMessages.LIKE_CANCEL), userId, filmId);
-        return filmStorage.getById(filmId);
+        return storage.getById(filmId);
     }
 
     public List<Film> getPopular(int count) {
-        return filmStorage.getAll().stream()
+        return storage.getAll().stream()
                 .sorted(LIKES_COMPARATOR)
                 .limit(count)
                 .collect(Collectors.toList());
--- src/main/java/ru/yandex/practicum/filmorate/service/user/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/user/UserService.java
@@ -1,26 +1,28 @@
 package ru.yandex.practicum.filmorate.service.user;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.excepions.ValidationException;
 import ru.yandex.practicum.filmorate.messages.LogMessages;
 import ru.yandex.practicum.filmorate.messages.ValidationExceptionMessages;
 import ru.yandex.practicum.filmorate.model.User;
-import ru.yandex.practicum.filmorate.storage.user.UserStorage;
+import ru.yandex.practicum.filmorate.service.AbstractService;
+import ru.yandex.practicum.filmorate.storage.Storage;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
 @Slf4j
-public class UserService {
-    private final UserStorage userStorage;
-
-    public UserService(UserStorage userStorage) {
-        this.userStorage = userStorage;
+public class UserService extends AbstractService<User> {
+    @Autowired
+    public UserService(Storage<User> storage) {
+        super.storage = storage;
     }
 
-    private User validate(User user) throws ValidationException {
+    @Override
+    public User validate(User user) throws ValidationException {
         if (user.getName() == null || user.getName().isBlank()) {
             log.info(ValidationExceptionMessages.LOGIN_TO_NAME.toString());
             user.setName(user.getLogin());
@@ -28,37 +30,15 @@ public class UserService {
         return user;
     }
 
-    public List<User> getAll() {
-        return userStorage.getAll();
-    }
-
-    public User save(User user) {
-        validate(user);
-        return userStorage.save(user);
-    }
-
-    public User update(User user) {
-        validate(user);
-        return userStorage.update(user);
-    }
-
-    public User remove(User user) {
-        return userStorage.remove(user);
-    }
-
-    public User getById(int id) {
-        return userStorage.getById(id);
-    }
-
     public User addFriend(int userId, int friendId) {
         if (userId == friendId)
             throw new ValidationException(String.valueOf(ValidationExceptionMessages.FRIEND_TO_FRIEND));
         getById(userId);
         getById(friendId);
-        userStorage.getById(userId).getFriends().add(friendId);
-        userStorage.getById(friendId).getFriends().add(userId);
+        storage.getById(userId).getFriends().add(friendId);
+        storage.getById(friendId).getFriends().add(userId);
         log.info(String.valueOf(LogMessages.FRIEND_DONE), userId, friendId);
-        return userStorage.getById(userId);
+        return storage.getById(userId);
     }
 
     public User removeFriend(int userId, int friendId) {
@@ -66,28 +46,28 @@ public class UserService {
             throw new ValidationException(String.valueOf(ValidationExceptionMessages.FRIEND_TO_FRIEND));
         getById(userId);
         getById(friendId);
-        if (!userStorage.getById(userId).getFriends().contains(friendId) && !userStorage.getById(friendId).getFriends().contains(userId))
+        if (!storage.getById(userId).getFriends().contains(friendId) && !storage.getById(friendId).getFriends().contains(userId))
             throw new ValidationException(String.valueOf(ValidationExceptionMessages.NOT_FRIENDS));
-        userStorage.getById(userId).getFriends().remove(friendId);
-        userStorage.getById(friendId).getFriends().remove(userId);
+        storage.getById(userId).getFriends().remove(friendId);
+        storage.getById(friendId).getFriends().remove(userId);
         log.info(String.valueOf(LogMessages.FRIEND_CANCEL), userId, friendId);
-        return userStorage.getById(userId);
+        return storage.getById(userId);
     }
 
     public List<User> getFriends(int userId) {
         getById(userId);
-        return userStorage.getById(userId).getFriends().stream()
-                .map(userStorage::getById)
+        return storage.getById(userId).getFriends().stream()
+                .map(storage::getById)
                 .collect(Collectors.toList());
     }
 
     public List<User> corporateFriends(int userId, int friendId) {
-        User user = userStorage.getById(userId);
-        User friend = userStorage.getById(friendId);
+        User user = storage.getById(userId);
+        User friend = storage.getById(friendId);
 
         List<User> mutualFriends = user.getFriends().stream()
                 .filter(friend.getFriends()::contains)
-                .map(userStorage::getById)
+                .map(storage::getById)
                 .collect(Collectors.toList());
 
         log.info(String.valueOf(LogMessages.LIST_OF_FRIENDS));
