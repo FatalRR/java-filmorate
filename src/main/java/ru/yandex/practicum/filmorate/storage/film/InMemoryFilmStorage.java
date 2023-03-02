f3f6dec36c5053bf7d683a03de287b8899ff1834
Sergey Malyunov <FatalRR@yandex.ru>
2023-03-02 20:02:10 +0700

    refactor: remove all methods

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/InMemoryFilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/InMemoryFilmStorage.java
@@ -1,42 +1,9 @@
 package ru.yandex.practicum.filmorate.storage.film;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
-import ru.yandex.practicum.filmorate.excepions.NotFoundException;
-import ru.yandex.practicum.filmorate.messages.LogMessages;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.storage.InMemoryStorage;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 @Component
-@Slf4j
 public class InMemoryFilmStorage extends InMemoryStorage<Film> implements FilmStorage {
-    @Override
-    public List<Film> getAll() {
-        return super.getAll();
-    }
-
-    @Override
-    public Film save(Film film) {
-        return super.save(film);
-    }
-
-    @Override
-    public Film update(Film film) {
-        return super.update(film);
-    }
-
-    @Override
-    public Film getById(int id) {
-        return super.getById(id);
-    }
-
-    @Override
-    public Film remove(Film film) {
-        return super.remove(film);
-    }
 }
--- src/main/java/ru/yandex/practicum/filmorate/storage/user/InMemoryUserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/InMemoryUserStorage.java
@@ -1,37 +1,9 @@
 package ru.yandex.practicum.filmorate.storage.user;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.InMemoryStorage;
 
-import java.util.List;
-
 @Component
-@Slf4j
 public class InMemoryUserStorage extends InMemoryStorage<User> implements UserStorage {
-    @Override
-    public List<User> getAll() {
-        return super.getAll();
-    }
-
-    @Override
-    public User save(User user) {
-        return super.save(user);
-    }
-
-    @Override
-    public User update(User user) {
-        return super.update(user);
-    }
-
-    @Override
-    public User getById(int id) {
-        return super.getById(id);
-    }
-
-    @Override
-    public User remove(User user) {
-        return super.remove(user);
-    }
 }
