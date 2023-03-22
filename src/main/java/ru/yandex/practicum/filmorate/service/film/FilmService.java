60b1a35bf3d7d590547d7ee6067a246dd5f50cb1
Sergey Malyunov <FatalRR@yandex.ru>
2023-03-22 10:09:11 +0700

    feat: add new storage "likeStorage", change calls methods

--- src/main/java/ru/yandex/practicum/filmorate/service/film/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/film/FilmService.java
@@ -10,6 +10,7 @@ import ru.yandex.practicum.filmorate.messages.LogMessages;
 import ru.yandex.practicum.filmorate.messages.ValidationExceptionMessages;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
+import ru.yandex.practicum.filmorate.storage.film.LikeStorage;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -19,9 +20,12 @@ import java.util.List;
 public class FilmService {
     private static final LocalDate BOUNDARY_DATE = LocalDate.of(1895, 12, 28);
     private final FilmStorage filmStorage;
+    private final LikeStorage likeStorage;
+
     @Autowired
-    public FilmService(FilmStorage filmStorage) {
+    public FilmService(FilmStorage filmStorage, LikeStorage likeStorage) {
         this.filmStorage = filmStorage;
+        this.likeStorage = likeStorage;
     }
 
     public List<Film> getAll() {
@@ -30,29 +34,29 @@ public class FilmService {
 
     public Film save(Film film) {
         validate(film);
-        log.info(String.valueOf(LogMessages.ADD),film);
+        log.info(String.valueOf(LogMessages.ADD), film);
         return filmStorage.save(film);
     }
 
     public Film update(Film film) {
         validate(film);
-        log.info(String.valueOf(LogMessages.UPDATE),film);
+        log.info(String.valueOf(LogMessages.UPDATE), film);
         return filmStorage.update(film);
     }
 
     public Film getById(Integer id) {
-        log.info(String.valueOf(LogMessages.GET), id );
+        log.info(String.valueOf(LogMessages.GET), id);
         return filmStorage.getById(id);
     }
 
     public void addLike(Integer filmId, Integer userId) {
-        filmStorage.addLike(filmId, userId);
+        likeStorage.addLike(filmId, userId);
         log.info(String.valueOf(LogMessages.LIKE_DONE), userId, filmId);
     }
 
     public void removeLike(Integer filmId, Integer userId) {
         if (filmId >= 1 && userId >= 1) {
-            filmStorage.removeLike(filmId, userId);
+            likeStorage.removeLike(filmId, userId);
             log.info(String.valueOf(LogMessages.LIKE_CANCEL), userId, filmId);
         } else {
             throw new NotFoundException(ExceptionMessages.POSITIVE_ID);
@@ -60,10 +64,10 @@ public class FilmService {
     }
 
     public List<Film> getPopular(Integer count) {
-      return filmStorage.getPopular(count);
+        return filmStorage.getPopular(count);
     }
 
-    public void validate (Film film) {
+    public void validate(Film film) {
         if (film.getReleaseDate().isBefore(BOUNDARY_DATE)) {
             throw new ValidationException(ValidationExceptionMessages.RELEASE_DATE.toString());
         }
