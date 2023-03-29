fc343eb0c9df35fee28260afe270da327d17ff99
Sergey Malyunov <FatalRR@yandex.ru>
2023-03-29 19:40:25 +0700

    feat: new log message

--- src/main/java/ru/yandex/practicum/filmorate/messages/LogMessages.java
+++ src/main/java/ru/yandex/practicum/filmorate/messages/LogMessages.java
@@ -21,7 +21,8 @@ public enum LogMessages {
     FRIEND_CANCEL("Пользователи с id: {} и {} больше не друзья"),
     LIKE_DONE("Пользователь с id: {} поставил лайк фильму с id: {}"),
     LIKE_CANCEL("Пользователь с id: {} удалил лайк у фильма с id: {}"),
-    LIST_OF_FRIENDS("Список общих друзей пользователей: {}");
+    LIST_OF_FRIENDS("Список общих друзей пользователей: {}"),
+    TRY_GET_DIRECTOR_FILM("Попытка получение списка фильмов режиссера directorId={}, sort={}.");
 
     private final String textLog;
 
