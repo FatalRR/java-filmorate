5d17be499a53332f26390d69ccab71b44681725e
Sergey Malyunov <FatalRR@yandex.ru>
2023-03-30 15:28:47 +0700

    feat: new mapper for director films

---
+++ src/main/java/ru/yandex/practicum/filmorate/mappers/FilmWithGenreAndDirectorMapper.java
@@ -0,0 +1,47 @@
+package ru.yandex.practicum.filmorate.mappers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
+import ru.yandex.practicum.filmorate.model.Director;
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.Genre;
+import ru.yandex.practicum.filmorate.model.Mpa;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@Component
+@Slf4j
+public class FilmWithGenreAndDirectorMapper implements RowMapper<Film> {
+    @Override
+    public Film mapRow(ResultSet rs, int rowNum) throws SQLException {
+        log.debug("/mapRowFilmWithGenreAndDirector");
+        Film film = Film.builder()
+                .id(rs.getInt("film_id"))
+                .name(rs.getString("name"))
+                .description(rs.getString("description"))
+                .releaseDate(rs.getDate("release_date").toLocalDate())
+                .duration(rs.getInt("duration"))
+                .mpa(Mpa.builder()
+                        .id(rs.getInt("mpa_id"))
+                        .name(rs.getString("mpa_name"))
+                        .build())
+                .build();
+
+        Director director = Director.builder()
+                .id(rs.getInt("director_id"))
+                .name(rs.getString("director_name"))
+                .build();
+        film.addFilmDirectors(director);
+
+        Genre genre = Genre.builder()
+                .id(rs.getInt("genre_id"))
+                .name(rs.getString("genre_name"))
+                .build();
+
+        film.addFilmGenre(genre);
+
+        return film;
+    }
+}
