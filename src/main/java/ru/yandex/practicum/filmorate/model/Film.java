07453e1f3d20ed515096b3679e382692ca8d3b6c
Sergey Malyunov <FatalRR@yandex.ru>
2023-04-02 21:09:07 +0700

    <code style>

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -26,7 +26,6 @@ public class Film extends Entity {
     private LocalDate releaseDate;
     @PositiveOrZero(message = ExceptionMessages.POSITIVE_DURATION)
     private long duration;
-
     @NotNull
     private Mpa mpa;
     private final Set<Genre> genres = new TreeSet<>(Comparator.comparingInt(Genre::getId));
